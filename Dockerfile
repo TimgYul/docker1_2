613af4c3cd6d84f60c7e5a9c6c9ebe86e71becb7
Yuliya Timganova <yulchik12-12y@yandex.ru>
2023-04-17 23:17:44 +0500

    4-ty

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM python:3.10
 
 COPY . /src
-COPY ./requirements.txt /src/requirements.txt
 WORKDIR  /src
 RUN chmod +x script.sh
-RUN pip3 install --no-cache-dir --upgrade -r /src/requirements.txt
+RUN pip3 install --no-cache-dir --upgrade -r requirements.txt
 
 EXPOSE 8000
 
--- logistic/views.py
+++ logistic/views.py
@@ -3,7 +3,6 @@ from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 
